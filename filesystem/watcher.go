ca2ca661db60f674afcb239f01174815f514a76b
Charles Burton <charles.d.burton@gmail.com>
2017-08-31 14:13:21 -0600

    Added Viper config and multithreaded SHA1 hasher

--- filesystem/watcher.go
+++ filesystem/watcher.go
@@ -1,40 +1,64 @@
 package filesystem
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
+	"io"
+	"io/ioutil"
 	"log"
+	"math"
 	"os"
 	"path/filepath"
 
 	"github.com/fsnotify/fsnotify"
 )
 
+var (
+	hashJobs    = make(chan string, 200)
+	hashResults = make(chan string, 1000)
+)
+
+const filechunk = 8192
+
 type WatchDirs struct {
 	Dirs []string
 }
 
-//Watches ...Recursively walk the filesystem
-func Watches(tops []string) error {
+type HashedFile struct {
+	Hash     string
+	FileName string
+}
+
+//Watches ...Recursively walk the filesystem, entrypoint to file watching
+func Watches(tops []string) {
+	startHashWorkerPool(8)
 	var dirs WatchDirs
 	for _, top := range tops {
-		filepath.Walk(top, func(path string, f os.FileInfo, err error) error {
+		err := filepath.Walk(top, func(path string, f os.FileInfo, err error) error {
 			if err != nil {
 				log.Println(err)
-				return nil
+				return err
 			}
-			log.Println("Path: ", path)
-			dirs.Dirs = append(dirs.Dirs, path)
+			//log.Println("File: ", path)
+			if f.IsDir() {
+				//log.Println("Path: ", path)
+				dirs.Dirs = append(dirs.Dirs, path)
+			}
 			return nil
 		})
+		if err != nil {
+			log.Println(err)
+		}
 		log.Println("Continuing Loop")
 	}
-	log.Println("Staring Dedup: ")
+	log.Println("Starting Dedup: ")
 	dirs.Dedup()
+	dirs.Index()
 	log.Println("Post Dedup: ")
 	for _, dir := range dirs.Dirs {
 		log.Println(dir)
 	}
 	dirs.Watch()
-	return nil
 }
 
 //Dedup ...Remove all duplicate entires from configured directories
@@ -67,6 +91,9 @@ func (dirs *WatchDirs) Watch() {
 				log.Println("event:", event)
 				if event.Op&fsnotify.Write == fsnotify.Write {
 					log.Println("modified file:", event.Name)
+					/* var m runtime.MemStats
+					runtime.ReadMemStats(&m)
+					log.Printf("\nAlloc = %v\nTotalAlloc = %v\nSys = %v\nNumGC = %v\n\n", m.Alloc/1024, m.TotalAlloc/1024, m.Sys/1024, m.NumGC) */
 				}
 			case err := <-watcher.Errors:
 				log.Println("error:", err)
@@ -86,8 +113,64 @@ func (dirs *WatchDirs) Watch() {
 
 //Index ...Index all the files currently in the system
 func (dirs *WatchDirs) Index() {
+	for _, dir := range dirs.Dirs {
+		files, _ := ioutil.ReadDir(dir)
+		for _, file := range files {
+			if !file.IsDir() {
+				hashJobs <- dir + "/" + file.Name()
+			}
+		}
+	}
+
 }
 
 //Synchronize ...Synchronize Index with Backblaze
 func (dirs *WatchDirs) Synchronize() {
+}
+
+func startHashWorkerPool(workers int) {
+	for i := 1; i <= workers; i++ {
+		go hashWorker(i, hashJobs, hashResults)
+	}
+}
+
+func hashWorker(id int, jobs <-chan string, results chan<- string) {
+	for job := range jobs {
+		log.Println("Worker: ", id, "started job: ", job)
+		hash, err := hashFile(job)
+		if err != nil {
+			log.Println(err)
+			//results <- hash
+		}
+		log.Println("Hash: ", hash)
+	}
+}
+
+//Creat a sha1 of a file of unknown size
+func hashFile(file string) (string, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+	defer f.Close()
+
+	//get information about the file
+	info, err := f.Stat()
+	if err != nil {
+		return "", err
+	}
+	size := info.Size()
+
+	blocks := uint64(math.Ceil(float64(size) / float64(filechunk)))
+	hash := sha1.New()
+
+	for i := uint64(0); i < blocks; i++ {
+		blocksize := int(math.Min(filechunk, float64(size-int64(i*filechunk))))
+
+		buf := make([]byte, blocksize)
+		f.Read(buf)
+		io.WriteString(hash, string(buf))
+	}
+	return hex.EncodeToString(hash.Sum(nil)), nil
 }
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 
 	"github.com/boltdb/bolt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
 )
 
 func main() {
@@ -17,8 +19,27 @@ func main() {
 		log.Fatal(err)
 	}
 	defer database.BoltConn.Close()
-	dirs := []string{"/tmp"}
+	viper.SetConfigType("yaml")
+	viper.SetConfigName("config")
+	viper.AddConfigPath("/etc/backblaze-backup")
+	viper.AddConfigPath("$HOME/.backblaze-backup")
+	viper.AddConfigPath(".")
+	err = viper.ReadInConfig()
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		log.Println("Config changed: ", e.Name)
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	//accountId := viper.Get("account-id")
+	//applicationKey := viper.Get("application-key")
+	watchDirs := viper.GetStringSlice("watch-dirs")
+	log.Println(watchDirs)
+	filesystem.Watches(watchDirs)
+	/*dirs := []string{"/tmp"}
+
 	go filesystem.Watches(dirs)
 	for {
-	}
+	}*/
 }
