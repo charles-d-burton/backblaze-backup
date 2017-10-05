d74eba3e30b60f989a2db47df8847f847e15cd75
charles <charles.d.burton@gmail.com>
2017-10-05 16:36:11 -0600

    finished function to create backblaze bucket

--- requests/create-bucket.go
+++ requests/create-bucket.go
@@ -1,17 +1,34 @@
 package requests
 
 import (
+	"fmt"
 	"log"
+	"os"
+
+	"gopkg.in/resty.v0"
 
 	"github.com/spf13/viper"
 )
 
+const b2CreateBucket = "/b2api/v1/b2_create_bucket"
+
 func CreateBackblazeBucket() error {
 	authorization, err := GetAuthorization(viper.GetString("account-id"), viper.GetString("application-key"))
+	name, err := os.Hostname()
+	log.Println("HostName: ", name)
 	if err != nil {
 		log.Println(err)
 	}
-	log.Println("Token: ", authorization.AuthorizationToken)
+	request, err := resty.R().
+		SetQueryParams(map[string]string{
+		"accountId":  viper.GetString("account-id"),
+		"bucketName": name,
+		"bucketType": "allPrivate",
+	}).
+		SetHeader("Accept", "application/json").
+		SetHeader("Authorization", authorization.AuthorizationToken).
+		Get(authorization.APIURL + b2CreateBucket)
+	fmt.Printf("\nResponse Body: %v", string(request.Body()))
 
 	return err
 }
